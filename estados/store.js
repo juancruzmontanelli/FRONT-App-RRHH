77e68d2f01c605e96181d549ac1328d1d7c5b4d8
Bstar098 <luceropintos@abc.gob.ar>
2022-11-17 16:09:04 -0300

    configuracion redux: store

--- estados/store.js
+++ estados/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import logger from "redux-logger";
 import usuarios from "./usuarios";
+import novedades from "./novedades";
 
 const store = configureStore({
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
   reducer: {
     usuarios,
+    novedades
   },
 });
 
