a4a223fa488e562914515f10732a54d6286637a1
Bstar098 <luceropintos@abc.gob.ar>
2022-11-18 12:11:37 -0300

    Configuración acciones asincrónicas de novedades, redux

--- estados/usuarios.js
+++ estados/usuarios.js
@@ -8,7 +8,7 @@ const estadoInicial = {
 };
 
 export const urlBaseUsuario = axios.create({
-  baseURL: "http://192.168.1.41:8080/api/usuarios",
+  baseURL: `http://127.0.0.1:8080/api/usuarios`,
 });
 
 export const iniciarSesion = createAsyncThunk(
