593f99e0aa6881b23486b36cbbdaea8100f969ed
Bstar098 <luceropintos@abc.gob.ar>
2022-12-05 16:03:00 -0300

    Vista, acción de redux y funcionalidad de crear equipo, usuario RRHH

--- navigation/StackNavigation.js
+++ navigation/StackNavigation.js
@@ -8,6 +8,7 @@ import { useDispatch } from "react-redux";
 import { Button } from "@react-native-material/core";
 import { cerrarSesion } from "../estados/usuarios";
 import HistorialAsistencias from "../screens/HistorialAsistencias";
+
 import MiembrosLista from "../screens/MiembrosLista";
 import EquiposLista from "../screens/EquiposLista";
 import MiembroScreen from "../screens/MiembroScreen";
@@ -17,8 +18,7 @@ import { Alert } from "react-native";
 import Oficinas from "../screens/OficinasScreen";
 import crearOficina from "../screens/CrearOficina";
 import SuperUserSolicitudes from "../screens/superUserSolicitudes";
-
-
+import CrearEquipo from "../screens/CrearEquipo";
 
 const StackScreen = (navigation) => {
   const Stack = createNativeStackNavigator();
@@ -66,18 +66,23 @@ const StackScreen = (navigation) => {
       />
       <Stack.Screen name="GLOBAL NEWS" component={Login} />
 
-
-     
       <Stack.Screen name="Mi Equipo" component={MiembrosLista} />
       <Stack.Screen name="Equipos" component={EquiposLista} />
+      <Stack.Screen name="Crear Equipo" component={CrearEquipo} />
       <Stack.Screen name="Miembro">
         {(props) => <MiembroScreen {...props} />}
       </Stack.Screen>
-      <Stack.Screen name="HistorialAsistencias" component={HistorialAsistencias} />
-      <Stack.Screen name="HistorialNovedades" component={HistorialNovedades} />
+      <Stack.Screen
+        name="HistorialAsistencias"
+        component={HistorialAsistencias}
+      />
+      {/*       <Stack.Screen name="HistorialNovedades" component={HistorialNovedades} /> */}
       <Stack.Screen name="Novedades" component={Novedades} />
       <Stack.Screen name="VerSolicitudes" component={VerSolicitudes} />
-      <Stack.Screen name="SuperUsuarioNovedades" component={SuperUserSolicitudes} />
+      <Stack.Screen
+        name="SuperUsuarioNovedades"
+        component={SuperUserSolicitudes}
+      />
       <Stack.Screen name="Novedad">
         {(props) => <NovedadScreen {...props} novedad={novedad} />}
       </Stack.Screen>
